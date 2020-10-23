cf5c26b7a70a9da4a6c80d2b1086613db996825f
Neeraj Kashyap <nkashy1@gmail.com>
2020-10-22 23:41:02 -0700

    Split interface between git and parse
    mypy: passed
    black: passed

--- locust/cli.py
+++ locust/cli.py
@@ -18,28 +18,14 @@ def generate_argument_parser() -> argparse.ArgumentParser:
         description="Locust: Analyze Python code across git references",
         epilog=f"Version {version.LOCUST_VERSION}",
     )
-    parser.add_argument(
-        "-r", "--repo", required=False, default=".", help="Path to git repository"
-    )
-    parser.add_argument(
-        "initial",
-        nargs="?",
-        default=None,
-        help="Initial git revision",
-    )
-    parser.add_argument(
-        "terminal",
-        nargs="?",
-        default=None,
-        help="Terminal git revision",
-    )
+    git.populate_argument_parser(parser)
     parser.add_argument(
         "-o",
         "--output",
         required=False,
         type=argparse.FileType("w"),
         default=sys.stdout,
-        help="Path to which to write results (as JSON)",
+        help="Path to which to write results",
     )
     parser.add_argument(
         "-f",
@@ -65,18 +51,10 @@ def main():
     parser = generate_argument_parser()
     args = parser.parse_args()
 
-    repo = git.get_repository(args.repo)
-
-    initial_ref = repo.revparse_single("HEAD").short_id
-    if args.initial is not None:
-        initial_ref = repo.revparse_single(args.initial).short_id
-
-    terminal_ref = None
-    if args.terminal is not None:
-        terminal_ref = repo.revparse_single(args.terminal).short_id
-
-    patches = git.get_patches(repo, initial_ref, terminal_ref)
-    visitor = parse.LocustVisitor(repo, terminal_ref, patches)
+    git_results = git.run(args.repo, args.initial, args.terminal)
+    visitor = parse.LocustVisitor(
+        git_results.repo, git_results.terminal_ref, git_results.patches
+    )
     changes = visitor.parse_all()
     normalized_changes = [
         render.repo_relative_filepath(args.repo, change) for change in changes
@@ -84,9 +62,13 @@ def main():
 
     nested_results = render.nest_results(normalized_changes)
     results = render.results_dict(nested_results)
-    results = render.enrich_with_refs(results, initial_ref, terminal_ref)
+    results = render.enrich_with_refs(
+        results, git_results.initial_ref, git_results.terminal_ref
+    )
     if args.github is not None and args.terminal is not None:
-        results = render.enrich_with_github_links(results, args.github, terminal_ref)
+        results = render.enrich_with_github_links(
+            results, args.github, git_results.terminal_ref
+        )
     renderer = render.renderers[args.format]
     results_string = renderer(results)
     with args.output as ofp:
